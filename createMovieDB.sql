799200e84c60c27dceca35c2dc7dd31486900036
ajhickman <96302079+ajhickman@users.noreply.github.com>
2022-06-28 17:56:49 -0400

    SQL Code to create the database

---
+++ createMovieDB.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Movie] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50)  NOT NULL,
+    [Description] NVARCHAR (500) NULL,
+    [Year]        INT            NOT NULL,
+    CONSTRAINT [PK_Movie] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
